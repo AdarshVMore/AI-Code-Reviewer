af015003161b0ccc5a9557acfd4a86261c411075
AdarshVMore <skullcrushermore@gmail.com>
2026-07-25 12:45:00 +0530

    leave myself a note on what this buffer/zip stuff actually is

--- Server/app/worker-service/log-fetchers/github.logs.ts
+++ Server/app/worker-service/log-fetchers/github.logs.ts
@@ -11,9 +11,11 @@ export async function fetchGithubActionLogs(installationId: number, owner: strin
   });
 
   const buffer = Buffer.from(response.data as ArrayBuffer);
+  // here buffer is like a bunch of alphaNumbers like <Buffer 50 4b 03 04 14 00 08 00 ...>
   const zip = new AdmZip(buffer);
   const entries = zip.getEntries();
-
+  // here this entries return folders and files with extension like [ ZipEntry, ZipEntry, ZipEntry]
+ 
   let logs = "";
   for (const entry of entries) {
     if (!entry.isDirectory) {
